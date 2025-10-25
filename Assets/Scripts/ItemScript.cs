5ab66564425624c89e17d9ebfb00649daf03aad2
SINCLAIR59 <kongyot.daengngam@e-tech.ac.th>
2025-10-25 18:04:08 +0700

    Add project

---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemScript : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public GameObject interactUI; // UI ปุ่ม E
+    private bool canInteract = false;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // ตรวจสอบ UI ก่อนซ่อน
+        if (interactUI != null)
+        {
+            // ถ้าเป็น Child ของ Item ให้ตัดออกจาก Parent
+            interactUI.transform.SetParent(null);
+            interactUI.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUpItem();
+        }
+    }
+
+    private void PickUpItem()
+    {
+        Debug.Log("เก็บไอเท็มแล้ว!");
+        // ซ่อน UI ก่อนทำลาย Item
+        if (interactUI != null)
+            interactUI.SetActive(false);
+
+        Destroy(gameObject); // ลบ Item
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canInteract = true;
+
+            if (interactUI != null)
+                interactUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canInteract = false;
+
+            if (interactUI != null)
+                interactUI.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using static UnityEngine.GraphicsBuffer;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float MoveX, MoveY, Speed;
+    private Rigidbody2D RB;
+    public bool isCollider;
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MoveX = Input.GetAxis("Vertical");
+        RB.linearVelocity = new Vector2(MoveX * Speed, RB.linearVelocity.y);
+        MoveY = Input.GetAxis("Horizontal");
+        RB.linearVelocity = new Vector2(MoveY * Speed, RB.linearVelocity.x);
+    }
+
+    private void OnTriggerEnter2D(Collider2D Target)
+    {
+        if (Target.gameObject.CompareTag("Item"))
+        {
+            isCollider = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D Target)
+    {
+        if (Target.gameObject.CompareTag("Item"))
+        {
+            isCollider = false;
+        }
+    }
+}
